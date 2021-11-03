5a183e27c99064faef3c612792db594a5cc1eabb
GinoGreen <luigiverde97@gmail.com>
2021-11-03 15:03:06 +0100

    ripresa

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ myContainer.innerHTML = `
 
 // inizializzo la costante del numero di bombe presenti nella griglia
 const BOMBS_NUMBER = 16;
+let bombs = [];
 
 
 //genero la griglia una volta cliccato il bottone PLAY
@@ -26,8 +27,8 @@ playButton.addEventListener('click', function () {
 
    // inizializzo un array di bombe che conterrá 'BOMBS_NUMBER' numeri
    // riempio l'array bombs
-   const bombs = getBombNumbers(BOMBS_NUMBER);
-   console.log(bombs);
+   bombs = getBombNumbers(valueDifficult);
+   console.log(`array di bombe lungo ${BOMBS_NUMBER} elementi:`, bombs);
    
    //per generare la griglia ho bisogno di passare alla funzione un argomento che indichi la dimensione della stessa(griglia), dipendentemente dalla difficoltá selezionata
    initGrid(getNumberByValue(valueDifficult));
@@ -35,44 +36,47 @@ playButton.addEventListener('click', function () {
 
 });
 
-
 //*********FUNZIONI********** */
 
 //funzione che genera la visualizzazione della griglia
-function initGrid(value) {
+function initGrid(numberSquare) {
    
    //cicolo che crea TOT quadrati dipendentemente dalla difficoltá scelta
-   for (let i = 0; i < value; i++) {
+   for (let i = 0; i < numberSquare; i++) {
       
       //creo l'elemento square e lo aggiungo dentro al container
-      const square = createSquare(myContainer);
-      //imposto la classe pre esistente al div square
-      square.className = ('square');
-      //aggiungo la classe allo square dopo aver controllato il livello di difficoltá
-      switch (value) {
-         case 49:
-            square.classList.add('crazy');
-            break;
-
-         case 81:
-            square.classList.add('hard');
-            break;
-      
-         case 100:
-            square.classList.add('easy');
-            break;
-      }
+      const square = createSquare(numberSquare);
       //inserisco il numbero indice+1 dentro lo square
       square.innerHTML = (i+1);
       
+      //aggiungo un evento click a tutti gli square
+      square.addEventListener('click', handleClickSquare);
+
    }
 }
 
 //funzione che crea quadrati 'square' e li inserire nel container (target)
-function createSquare(target) {
+function createSquare(numberSquare) {
    
    const square = document.createElement('div');
-   target.append(square);
+   //imposto la classe pre esistente al div square
+   square.className = ('square');
+   //aggiungo la classe allo square dopo aver controllato il livello di difficoltá
+   switch (numberSquare) {
+      case 49:
+         square.classList.add('crazy');
+         break;
+
+      case 81:
+         square.classList.add('hard');
+         break;
+   
+      case 100:
+         square.classList.add('easy');
+         break;
+   }
+
+   myContainer.append(square);
 
    return square;
 }
@@ -91,18 +95,22 @@ function getNumberByValue(difficult) {
    return 49;
 }
 
-// funzione che genera una array di N numeri
-function getBombNumbers(n) {
+/**
+ * funzione che genera una array di 'BOMBER_NUMBER' numeri da 1 a difficult
+ * @param {string}  difficult
+ * @returns 
+ */
+function getBombNumbers(difficult) {
 
    const array = [];
 
-   while (array.length < n) {
+   while (array.length < BOMBS_NUMBER) {
       
-      const bombNumber = getRandomNumber(1, getNumberByValue());
+      const bombNumber = getRandomNumber(1, getNumberByValue(difficult));
 
       if (!array.includes(bombNumber)) {
          array.push(bombNumber);
-         console.log('numero bomba inserito', bombNumber);
+         // console.log('numero bomba inserito', bombNumber);
       }
 
    }
@@ -110,8 +118,28 @@ function getBombNumbers(n) {
    return array;
  }
 
+// funzione che genera un numero casuale dato il range
 function getRandomNumber(min, max) {
     
    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-// funzione che genera numeri casuali
+
+/**
+ * funzione che verifica se il numero contenuto all'interno dello square é incluso nell'array bombs
+ * @param {Event} event 
+ */
+function handleClickSquare(event) {
+   
+   console.log('numero contenuto nello square', parseInt(event.target.innerText));
+   // console.log(this, 'clicked');
+   for (let i = 0; i < BOMBS_NUMBER; i++) {
+      
+      if (parseInt(event.target.innerText) === bombs[i]) {
+         
+         // return console.log('hai perso');
+      }
+      
+   }
+
+   // return console.log('continua');
+}
